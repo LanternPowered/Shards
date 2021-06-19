64180e33b81e3c683d7c1d395a55c1b140c9cbee
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2021-06-19 12:08:52 +0200

    Fix license header

--- core/src/commonMain/kotlin/org/lanternpowered/shards/util/GenericType.kt
+++ core/src/commonMain/kotlin/org/lanternpowered/shards/util/GenericType.kt
@@ -1,3 +1,12 @@
+/*
+ * Shards
+ *
+ * Copyright (c) LanternPowered <https://www.lanternpowered.org>
+ * Copyright (c) contributors
+ *
+ * This work is licensed under the terms of the MIT License (MIT). For
+ * a copy, see 'LICENSE.txt' or <https://opensource.org/licenses/MIT>.
+ */
 @file:Suppress("FunctionName")
 
 package org.lanternpowered.shards.util
